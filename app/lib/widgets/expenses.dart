1c45ed345229fab452ab8bbd8130dc9e8dbfa36e
susumu2357 <susumu.0112357@gmail.com>
2024-02-19 22:34:16 +0100

    add overlay input

---
+++ app/lib/widgets/expenses.dart
@@ -0,0 +1,58 @@
+import 'package:app/widgets/expenses_list/expenses_list.dart';
+import 'package:app/models/expense.dart';
+import 'package:app/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.9,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.6,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return const NewExpense();
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add)),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ app/lib/widgets/new_expense.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:app/models/expense.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _NewExpense();
+  }
+}
+
+class _NewExpense extends State<NewExpense> {
+  final _titleContoroller = TextEditingController();
+  final _amountContoroller = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleContoroller.dispose();
+    _amountContoroller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleContoroller,
+            maxLength: 50,
+            decoration: const InputDecoration(label: Text('Title')),
+          ),
+          Row(children: [
+            Expanded(
+              child: TextField(
+                controller: _amountContoroller,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  prefixText: '\$',
+                  label: Text('Amount'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    _selectedDate == null
+                        ? 'No date selected'
+                        : formatter.format(_selectedDate!),
+                  ),
+                  IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(
+                      Icons.calendar_month,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleContoroller.text);
+                  print(_amountContoroller.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
